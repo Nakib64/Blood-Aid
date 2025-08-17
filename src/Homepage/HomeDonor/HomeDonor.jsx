359fabe245676dd375639db38aa216469db70c4d
Nakib64 <nafiz2282@gmail.com>
2025-08-17 20:22:15 +0600

    added donation request page

--- src/Homepage/HomeDonor/HomeDonor.jsx
+++ src/Homepage/HomeDonor/HomeDonor.jsx
@@ -24,7 +24,7 @@ export default function HomeDonors() {
     keepPreviousData: true,
   });
  
-  return (
+  return ( 
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
